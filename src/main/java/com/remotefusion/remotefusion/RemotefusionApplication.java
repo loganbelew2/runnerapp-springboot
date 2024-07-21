e5ad40a913b12d1c71485472b0b4d2edf6f71419
Logan Belews <Loganbelew2@gmail.com>
2024-07-20 20:19:34 -0500

    hey there

---
+++ src/main/java/com/remotefusion/remotefusion/RemotefusionApplication.java
@@ -0,0 +1,13 @@
+package com.remotefusion.remotefusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotefusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotefusionApplication.class, args);
+	}
+
+}
